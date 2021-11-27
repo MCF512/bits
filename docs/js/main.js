57981b3dc349194d56aeb5840419cd2c9c306cef
MCF512 <egdem4enko@yandex.ru>
2021-11-27 20:03:45 +0300

    first

---
+++ docs/js/main.js
@@ -0,0 +1,30 @@
+$(function () {
+
+});
+
+$('.team__slider').slick({
+  arrows: false,
+  dots: true,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 10000
+});
+
+$('.achive__slider').slick({
+  // centerMode: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 4000
+});
+
+$('.feedback__slider').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 10000
+});
